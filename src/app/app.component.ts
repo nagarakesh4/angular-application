c3154c2c5605d5510a759583bda50723061c7e48
Venkata Mani Naga Rakesh Buddhiraju <nagarakesh4@gmail.com>
2017-09-17 19:19:47 -0700

    Add first component and notes related to the different steps to define and behind the scenes for component

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+//import component decorator from angular core to use in decorator
+import { Component } from '@angular/core';
+
+//component decorator - Metadata
+@Component({
+  //selector - can be used as a directive
+  selector: 'pm-root', //prefix with something (pm) to identify part of app, root represents the component
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <div> Hello World Venkata's first Angular app (Component) </div>
+    </div>
+  `
+})
+
+//component class
+export class AppComponent{
+  pageTitle: string = 'Angular Venkata';
+}
