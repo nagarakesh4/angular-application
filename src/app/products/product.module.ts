2244457ea0d3faecc201946c23223777d0f6b319
Venkata Mani Naga Rakesh Buddhiraju <nagarakesh4@gmail.com>
2017-10-02 06:51:36 -0700

    add shared module to share common features - star-comp, commonModule, formsModule, convertToSpacesPipe

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,28 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
-import { ConvertToSpacesPipe } from '../shared/convert-to-space.pipe';
-import { StarComponent } from '../shared/star.component';
 import { ProductService } from './products.service';
 import { ProuductGuardService } from './prouduct-guard.service';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule, //for ngif and ngfor
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id', canActivate: [ProuductGuardService], component: ProductDetailComponent }
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
-    ProductDetailComponent,
-    ConvertToSpacesPipe,
-    StarComponent
+    ProductDetailComponent
   ],
   providers: [
     ProductService,
